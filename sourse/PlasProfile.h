97ff32a6c101418b622c10c2321ea493d42d33da
kohmab <kohmab@gmail.com>
2020-02-04 15:39:54 +0300

    change pertubation generation algorythm

--- sourse/PlasProfile.h
+++ sourse/PlasProfile.h
@@ -9,27 +9,21 @@ using namespace arma;
 
 struct MyPerturbation
 {
-    int Hx;
-    int Hz;
+    int Nh;
+    int Nw;
     int Nz;
     int Nx;
     double Amplitude;
     Mat<double> Shape;
     Col<double> ZShape;
-    MyPerturbation(int iNx, int iNz, int iHx, int iHz, double iAmplitude, cx_rowvec x, cx_colvec z, double Lf)
+    MyPerturbation(int iNx, int iNz, int iNh, int iNw, double iAmplitude, cx_rowvec x, cx_colvec z, double Lf)
     {
-        Hx = iHx;
-        Hz = iHz;
+        Nw = iNw;
+        Nh = iNh;
         Nz = iNz;
         Nx = iNx;
         Amplitude = iAmplitude;
         Shape.zeros(Nz, Nx);
-
-        Mat<double> pert_z_Shape,
-            pert_x_Shape;
-
-        pert_x_Shape.zeros(Nz, Nx);
-        pert_z_Shape.zeros(Nz, Nx);
 
         // Shape of the z-enevelope;
         ZShape.zeros(Nz);
@@ -39,89 +33,67 @@ struct MyPerturbation
                 ZShape(i) = real(sin(M_PI * (z(i) - Lf) / 2.0 / Lf) * sin(M_PI * (z(i) - Lf) / 2.0 / Lf));
         }
 
-        // X-shape of perturbation;
         cx_double dx = x(1) - x(0), Lx = (x(Nx - 1) - x(0) + dx);
-        if (Hx == 0 && abs(Hz) > 0)
+
+        if (Nw == 0)
         {
-            pert_x_Shape.ones(Nz, Nx);
-        }
-        if (Hx <= -1)
-        {
-            Mat<double> Rand;
-            arma_rng::set_seed_random();
-            Rand.randu(1, 1);
-            pert_x_Shape.each_row() = -real(cos(2.0 * M_PI * Rand(0) + Hx * 2.0 * M_PI * (x + dx / 2.0 - x(0)) / Lx));
-        }
-        if (Hx == 1)
-        {
-            pert_x_Shape.each_row() = -real(cos(2.0 * M_PI * (x + dx / 2.0 - x(0)) / Lx));
-        }
-        if (Hx > 1 && Hx < 100)
-        {
-            Mat<double> Rand;
-            arma_rng::set_seed_random();
-            Rand.randu(1, Hx);
-            for (int j = 0; j < Hx; j++)
+            if (Nh == 0)
             {
-                pert_x_Shape.each_row() += -real(cos(2.0 * j * M_PI * (x + dx / 2.0 - x(0)) / Lx + 2.0 * M_PI * Rand(j)));
-                //cout << Rand(j) << endl;
+                Shape.ones(Nz, Nx);
+            }
+            if (Nh <= -1)
+            {
+                Mat<double> Rand;
+                arma_rng::set_seed_random();
+                Rand.randu(1, 1);
+                Shape.each_row() = -real(cos(2.0 * M_PI * Rand(0) + Nh * 2.0 * M_PI * (x + dx / 2.0 - x(0)) / Lx));
+            }
+
+            if (Nh == 1)
+            {
+                Shape.each_row() = -real(cos(2.0 * M_PI * (x + dx / 2.0 - x(0)) / Lx));
+            }
+            if (Nh > 1)
+            {
+                Mat<double> Rand;
+                arma_rng::set_seed_random();
+                Rand.randu(1, Nh);
+                for (int j = 0; j < Nh; j++)
+                {
+                    Shape.each_row() += -real(cos(2.0 * j * M_PI * (x + dx / 2.0 - x(0)) / Lx + 2.0 * M_PI * Rand(j)));
+                    //cout << Rand(j) << endl;
+                }
+            }
+            if (Nh == 100)
+            {
+                Shape.each_row() = pow(real(0.5 - 0.5 * cos(2.0 * M_PI * (x + dx / 2.0 - x(0)) / Lx)), 100);
+            }
+            if (Nh == 101)
+            {
+                Mat<double> Rand;
+                arma_rng::set_seed_random();
+                Rand.randu(1, 1);
+                Shape.each_row() = pow(real(0.5 - 0.5 * cos(2.0 * M_PI * (x + dx / 2.0 - x(0)) / Lx + 2.0 * M_PI * Rand(0))), 100);
             }
         }
-        if (Hx == 100)
+        else
         {
-            pert_x_Shape.each_row() = pow(real(0.5 - 0.5 * cos(2.0 * M_PI * (x + dx / 2.0 - x(0)) / Lx)), 100);
-        }
-        if (Hx == 101)
-        {
-            Mat<double> Rand;
-            arma_rng::set_seed_random();
-            Rand.randu(1, 1);
-            pert_x_Shape.each_row() = pow(real(0.5 - 0.5 * cos(2.0 * M_PI * (x + dx / 2.0 - x(0)) / Lx + 2.0 * M_PI * Rand(0))), 100);
-        }
-        // Z-shape of perturbation;
-        cx_double dz = z(1) - z(0);
-
-        if (Hz == 0)
-        {
-            pert_z_Shape.ones(Nz, Nx);
-        }
-        if (Hz <= -1)
-        {
-            Mat<double> Rand;
-            arma_rng::set_seed_random();
-            Rand.randu(1, 1);
-            pert_z_Shape.each_col() = -real(cos(2.0 * M_PI * Rand(0) + Hz * 2.0 * M_PI * (z + dz / 2.0) / Lx));
-        }
-        if (Hz == 1)
-        {
-            pert_z_Shape.each_col() = -real(cos(2.0 * M_PI * (z + dz / 2.0) / Lx));
-        }
-        if (Hz > 1 && Hz < 100)
-        {
-            Mat<double> Rand;
-            arma_rng::set_seed_random();
-            Rand.randu(1, Hz);
-            for (int j = 0; j < Hz; j++)
+            mat X2D(Nz, Nx, fill::zeros), Z2D(Nz, Nx, fill::zeros);
+            X2D.each_row() = real(x);
+            Z2D.each_col() = real(z);
+            for (int h = 1; h <= Nh; h++)
             {
-                pert_z_Shape.each_col() += -real(cos(2.0 * j * M_PI * (z + dz / 2.0) / Lx + 2.0 * M_PI * Rand(j)));
+                vec phase(Nw, fill::randu), angle(Nw, fill::randu);
+                phase *= 2.0 * M_PI;
+                angle *= M_PI;
+                double k = 2 * M_PI / real(Lx) * (double)h;
+                for (int w = 0; w < Nw; w++)
+                {
+                    Shape += cos(k * (X2D * cos(angle(w)) + Z2D * sin(angle(w))) + phase(w));
+                }
             }
         }
-        if (Hz == 100)
-        {
-            int Nz1 = 0, Nz2 = Nz - 1;
 
-            while (abs(z(Nz1)) > abs(Lx/2.0))
-                Nz1++;
-
-            while (abs(z(Nz2)) > abs(Lx/2.0))
-                Nz2--;
-
-            for (Nz1; Nz1 <= Nz2; Nz1++) {             
-                double val = pow(real(0.5 + 0.5 * cos(2.0 * M_PI * (z(Nz1)) / Lx)), 100);
-                pert_z_Shape.row(Nz1).for_each([val](double &V) { V = val; }); } //
-        }
-
-        Shape = pert_z_Shape % pert_x_Shape;
         if (Shape.max() > 0.0)
             Shape = Shape / Shape.max() * Amplitude;
         for (int i = 0; i < Nx; i++)
